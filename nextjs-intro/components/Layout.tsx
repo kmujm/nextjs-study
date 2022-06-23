79767b482aebcc37bb48063dfc24daf86648a506
kmujm <pmini09@kookmin.ac.kr>
2022-06-23 09:11:08 +0900

    server side rendering

--- nextjs-intro/components/Layout.tsx
+++ nextjs-intro/components/Layout.tsx
@@ -7,4 +7,4 @@ export default function Layout({children}: any) {
             {children}
         </div> 
     </>)
-}
+} 
--- nextjs-intro/pages/index.tsx
+++ nextjs-intro/pages/index.tsx
@@ -1,23 +1,11 @@
 import {useEffect, useState} from "react";
 import HeadTitle from "../components/HeadTitle";
 
-export default function Home() {
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const {results} = await(
-                await fetch(`/api/movies`)
-            ).json();
-            console.log(results)
-            setMovies(results)
-        })();
-    }, [])
-
+export default function Home({ results }: any) {
     return (
         <div className="container">
             <HeadTitle title="Home"/> 
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map((movie) => (
+            {results?.map((movie) => (
               <div className="movie" key={movie.id}>
                 <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
                 <h4>{movie.original_title}</h4>
@@ -49,7 +37,15 @@ export default function Home() {
 
 // 이 부분에 작성된 코드는 client에게 보여지지 않음. server에서만 실행됨.
 export async function getServerSideProps() {
-  
+  const { results } = await(
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    }
+  }
+     
 }
 
 // jwt 인증 redux, toolkit
